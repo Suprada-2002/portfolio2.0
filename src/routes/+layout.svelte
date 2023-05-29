f082fbccb901b9c38f30554985546fed46c0d96f
Suprada-2002 <supradatripathy789@gmail.com>
2023-05-29 19:34:56 +0530

    adding highlight to navbar and layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,8 @@
-<nav>
+<script>
+    import Navbar from "./Navbar.svelte";
+</script>
+
+<Navbar />
 <div class="container">
-    <h3>Suprada Tripathy</h3>
-    <ul>
-       <li><a href="/">Home</a></li>
-       <li><a href="/about">About</a></li>
-       <li><a href="/projects">Projects</a></li>
-       <li><a href="/contact">Contact</a></li>
-    </ul>
+    <slot />
 </div>
-</nav>
-   
-   <slot />
-
-   <style> 
-nav{
-    background-color: rgb(32, 32, 30);
-    color: white;
-    padding: 0.5em;
-}
-
-  .container{
-    display:flex;
- }
-   ul{
-       display:flex;
-       margin: 0px;
-       margin-left: auto;
-       list-style: none;
-   }
-   
-   li{
-       margin-right: 10px;
-   }
-h3{
-    margin: 0px;
-    font-size: 1.3rem;
-}
-
-a{
-    text-decoration: none;
-    color: white;
-}
-   </style>
-
-
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,63 @@
+<script>
+  import {page} from '$app/stores';
+  $: routeId = $page.route.id;
+</script>
+
+<nav>
+    <div class="container">
+        <h3>Suprada Tripathy</h3>
+        <ul>
+           <li><a href="/" class:active={
+            routeId === '/'
+           }>Home</a></li>
+           <li><a href="/about" class:active={
+            routeId === '/about'
+           }>About</a></li>
+           <li><a href="/projects" class:active={
+            routeId === '/projects'
+           }>Projects</a></li>
+           <li><a href="/contact" class:active={
+            routeId === '/contact'
+           }>Contact</a></li>
+        </ul>
+    </div>
+    </nav>
+
+    
+       <style> 
+    nav{
+        background-color: rgb(32, 32, 30);
+        color: white;
+        padding: 0.5em;
+    }
+    
+      .container{
+        display:flex;
+     }
+       ul{
+           display:flex;
+           margin: 0px;
+           margin-left: auto;
+           list-style: none;
+       }
+       
+       li{
+           margin-right: 10px;
+       }
+    h3{
+        margin: 0px;
+        font-size: 1.3rem;
+    }
+    
+    a{
+        text-decoration: none;
+        color: rgb(190, 189, 189);
+    }
+
+    .active{
+        color:white;
+    }
+       </style>
+    
+    
+    
